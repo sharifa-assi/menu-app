dc49701970ddd4928143eb79a2eb7c930108ee19
sharifa-assi <sharifaalassi2001@gmail.com>
2022-12-02 19:33:46 +0200

    MongoDB Connection

---
+++ api/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+console.log('heyyy!');
+
+app.use("/",(req,res)=>{
+    console.log('ahlan')
+})
+
+app.listen("5000",()=>{
+    console.log("Hello and welcome!")
+})
